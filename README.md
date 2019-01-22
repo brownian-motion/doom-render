d101a305d303ee804388176eaae0b1bafce00e0a
JJ Brown <jack.brown.255@gmail.com>
2019-01-22 12:55:48 -0600

    I made a pretty square

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This is my custom implementation of a simple 3-D renderer, designed similarly to
 
 It is heavily inspired by (and loosely follows) **Bisqwit**'s Youtube Video [*Creating a Doom-style 3d engine in C*](https://www.youtube.com/watch?v=HQYsFshbkYw).
 
+While I'm getting familiar with using the `gfx` library, I'm following [this out-of-date tutorial](https://suhr.github.io/gsgt/) on how to use it.
+
 # License
 
 This project is filed under the [DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE](LICENSE.md).
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,20 @@ pub type ColorFormat = gfx::format::Srgba8;
 pub type DepthFormat = gfx::format::DepthStencil;
 
 const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+const WHITE: [f32; 3] = [1.0, 1.0, 1.0];
+const RED: [f32; 3] = [1.0, 0.0, 0.0];
+const GREEN: [f32; 3] = [0.0, 1.0, 0.0];
+const BLUE: [f32; 3] = [0.0, 0.0, 1.0];
+
 const WINDOW_SIZE: LogicalSize = LogicalSize { width: 800.0, height: 800.0 };
+
+const SQUARE: [Vertex; 4] = [
+    Vertex { pos: [0.5, -0.5], color: RED },
+    Vertex { pos: [-0.5, -0.5], color: GREEN },
+    Vertex { pos: [-0.5, 0.5], color: BLUE },
+    Vertex { pos: [0.5, 0.5], color: WHITE},
+];
+const INDICES: &[u16] = &[0, 1, 2, 2, 3, 0];
 
 
 pub fn main() {
@@ -24,12 +37,23 @@ pub fn main() {
     let context = glutin::ContextBuilder::new()
         .with_vsync(true);
 
-    let (window, mut device, mut factory, mut main_color, mut main_depth) =
+    let (window, mut device, mut factory, main_color, mut main_depth) =
         gfx_glutin::init::<ColorFormat, DepthFormat>(builder, context, &events_loop)
             .expect("Could not create window.");
 
 //    originally a command buffer, converted into an "Encoder" which wraps that in a backend-indpt way
     let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+    let pso = factory.create_pipeline_simple(
+        include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/rect_150.glslv")),
+        include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/rect_150.glslf")),
+        pipe::new(),
+    ).expect("Could not establish pipeline.");
+    let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&SQUARE, INDICES);
+    let mut data = pipe::Data {
+        vbuf: vertex_buffer,
+        out: main_color,
+    };
+
     let mut running = true;
     while running { // looping on mut bool so that we could break from inside a lambda by changing it, and we clean up reliably
         events_loop.poll_events(|event: glutin::Event| {
@@ -41,7 +65,7 @@ pub fn main() {
                         | CloseRequested =>
                             running = false,
                         Resized(_) => {
-                            gfx_glutin::update_views(&window, &mut main_color, &mut main_depth);
+                            gfx_glutin::update_views(&window, &mut data.out, &mut main_depth);
                         }
                         _ => ()
                     }
@@ -50,9 +74,23 @@ pub fn main() {
             }
         });
 
-        encoder.clear(&main_color, BLACK); // draw the background
+        encoder.clear(&data.out, BLACK); // draw the background
+        encoder.draw(&slice, &pso, &data);
         encoder.flush(&mut device); // sends the commands in the buffer to the GPU, so this loop is completely executed in the same frame
         window.swap_buffers().expect("Could apply the current buffer to the window. Aborting.");
         device.cleanup();
+    }
+}
+
+// defines variables and such in the target GPU language
+gfx_defines! {
+    vertex Vertex {
+        pos: [f32; 2] = "a_Pos",
+        color: [f32; 3] = "a_Color",
+    }
+
+    pipeline pipe {
+        vbuf: gfx::VertexBuffer<Vertex> = (),
+        out: gfx::RenderTarget<ColorFormat> = "Target0",
     }
 }
